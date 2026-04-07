7be8dc770cae449e238de0eaaca166ae541b3598
jayashriiSH <pcmj.jayashrii@gmail.com>
2026-04-07 10:51:28 +0530

    P1: Transaction sorting using Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class Transaction {
+    String id;
+    double fee;
+    String time;
+
+    Transaction(String id, double fee, String time) {
+        this.id = id;
+        this.fee = fee;
+        this.time = time;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Transaction> list = new ArrayList<>();
+
+        list.add(new Transaction("id1", 10.5, "10:00"));
+        list.add(new Transaction("id2", 25.0, "09:30"));
+        list.add(new Transaction("id3", 5.0, "10:15"));
+
+        // Bubble Sort
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Collections.swap(list, j, j + 1);
+                }
+            }
+        }
+
+        System.out.println("Sorted by fee:");
+        for (Transaction t : list) {
+            System.out.println(t.id + " " + t.fee);
+        }
+
+        // High fee
+        System.out.println("High Fee > 50:");
+        for (Transaction t : list) {
+            if (t.fee > 50) {
+                System.out.println(t.id);
+            }
+        }
     }
 }
